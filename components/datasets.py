2dcdd48f37f0e47940f643cf917cba95efd309d2
Phil <philipmathew101@gmail.com>
2024-04-23 14:17:17 -0400

    Added constants for better CIFAR transformations

--- components/datasets.py
+++ components/datasets.py
@@ -4,12 +4,25 @@ from typing import *
 from torch.utils.data import Dataset
 
 
+# Improves model performance (https://github.com/weiaicunzai/pytorch-cifar100)
+CIFAR_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
+CIFAR_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
+
+
 class MembershipInferenceDataset(Dataset):
     def __init__(
         self, 
         member_dataset: Dataset, 
         non_member_dataset: Dataset
     ) -> None:
+        """
+        Constructs a membership inference dataset from existing member and non-member datasets.
+
+        :param member_dataset: Dataset of members.
+        :type member_dataset: Dataset
+        :param non_member_dataset: Dataset of non-members.
+        :type non_member_dataset: Dataset
+        """
         super(MembershipInferenceDataset, self).__init__()
         self.member_ds, self.non_member_ds = member_dataset, non_member_dataset
     
@@ -23,7 +36,18 @@ class MembershipInferenceDataset(Dataset):
         return len(self.member_ds) + len(self.non_member_ds)
 
 
-def init_full_ds(ds_type: str) -> Tuple[Dataset, Dataset, int, int]:
+def init_full_ds(ds_type: str, use_random_train_aug: bool = False) -> Tuple[Dataset, Dataset, int, int]:
+    """
+    Initialize the full dataset and figure out the number of classes and input channels.
+
+    :param ds_type: Name of the dataset to use. See README.md for more details on available datasets.
+    :type ds_type: str
+    :param use_random_train_aug: Whether to use random training augmentations, defaults to False
+    :type use_random_train_aug: bool, optional
+    :raises ValueError: If ds_type is not one of the available datasets.
+    :return: The full training dataset, the test dataset, the number of classes, and the number of input channels
+    :rtype: Tuple[Dataset, Dataset, int, int]
+    """
     match ds_type:
         case 'MNIST':
             transform = transforms.Compose(
@@ -37,18 +61,28 @@ def init_full_ds(ds_type: str) -> Tuple[Dataset, Dataset, int, int]:
             test_ds = MNIST('./data', train=False, transform=transform, download=True)
             num_classes, in_channels = 10, 1
         case 'CIFAR10':
-            train_transform = transforms.Compose(
-                [
-                    transforms.ToTensor(), 
-                    transforms.RandomResizedCrop(size=(64, 64), antialias=True),
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                ]
-            )
+            if use_random_train_aug:
+                train_transform = transforms.Compose(
+                    [
+                        transforms.RandomCrop(32, padding=4),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomRotation(15),
+                        transforms.ToTensor(),
+                        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+                    ]
+                )
+            else:
+                train_transform = transforms.Compose(
+                    [
+                        transforms.ToTensor(),
+                        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+                    ]
+                )
             
             test_transform = transforms.Compose(
                 [
                     transforms.ToTensor(),
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
                 ]
             )
             
@@ -56,18 +90,28 @@ def init_full_ds(ds_type: str) -> Tuple[Dataset, Dataset, int, int]:
             test_ds = CIFAR10('./data', train=False, transform=test_transform, download=True)
             num_classes, in_channels = 10, 3
         case 'CIFAR100':
-            train_transform = transforms.Compose(
-                [
-                    transforms.ToTensor(), 
-                    transforms.RandomResizedCrop(size=(64, 64), antialias=True),
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                ]
-            )
+            if use_random_train_aug:
+                train_transform = transforms.Compose(
+                    [
+                        transforms.RandomCrop(32, padding=4),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomRotation(15),
+                        transforms.ToTensor(),
+                        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+                    ]
+                )
+            else:
+                train_transform = transforms.Compose(
+                    [
+                        transforms.ToTensor(),
+                        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+                    ]
+                )
             
             test_transform = transforms.Compose(
                 [
-                    transforms.ToTensor(), 
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                    transforms.ToTensor(),
+                    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
                 ]
             )
             
